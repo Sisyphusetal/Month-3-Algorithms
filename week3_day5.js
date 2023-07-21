75a84ae324971bcb85bfdecf89e20656f77f23e8
Kurt James Clausen <kurtjclausen@gmail.com>
2023-07-21 08:50:08 -0700

    algorithms

--- week3_day5.js
+++ week3_day5.js
@@ -62,18 +62,66 @@ const expected3 = 0; // broccoli seeds key doesn't exist in available ingredient
  * @param {Ingredients} available
  * @returns {number} Max servings of the recipe that can be made.
  */
-function getMaxServings(recipe, available) { 
-    let amount = [];
-    let lowest = 1;
-    for(let key1 in recipe) {
-        for(let key2 in available) {
-            if(key1 == key2) {
-                amount.push((available[key2]-recipe[key1]))
-            }
+
+
+//My attempt
+// function getMaxServings(recipe, available) { 
+//     let amount = [];
+//     let lowest = 1;
+//     for(let key1 in recipe) {
+//         for(let key2 in available) {
+//             if(key1 == key2) {
+//                 amount.push((available[key2]-recipe[key1]))
+//             }
+//         }
+//     }
+//     return amount;
+// }
+
+// Mark's Solution
+const howManyRecipes = (recipe, available) => {
+    const receipeKeys = Object.keys(recipe);
+    const availableKeys = Object.keys(available);
+    let servings = [];
+
+    for (index in receipeKeys) {
+        if (availableKeys.indexOf(receipeKeys[index]) === -1) {
+            return 0;
+        }
+        servings.push(Math.floor(available[receipeKeys[index]] / recipe[receipeKeys[index]]));
+    }
+
+    let minServings = servings[0];
+    for (index in servings) {
+        if (servings[index] < minServings) {
+            minServings = servings[index];
         }
     }
-    return amount;
-}
+    return minServings;
+};
+
+const recipe1 = {
+    "organic fat": 99,
+    "broccoli seeds": 1,
+    okra: 1,
+    potato: 1,
+    spicy: 5,
+    "gourmet memes": 4200,
+};
+
+const available1 = {
+    "organic fat": 990,
+    "broccoli seeds": 1,
+    okra: 10,
+    potato: 10,
+    spicy: 50,
+    "gourmet memes": 42000,
+    sugar: 9001,
+    spice: 5,
+    "everything nice": 1,
+    "triple point water": 5,
+};
+
 
 console.log(getMaxServings(recipe1, available1));
 console.log(getMaxServings(recipe1, available2));
